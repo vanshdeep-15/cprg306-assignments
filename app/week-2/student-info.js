fbfc88552116bcc3ad4987c9f694ffb7ddce9c47
vanshdeep-15 <VanshdeepSingh@edu.sait.ca>
2025-10-21 12:01:10 -0600

    Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+// app/page.js
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <p>
+        <Link href="/week-2">Go to Week 2</Link>
+      </p>
+    </div>
+  );
+}
